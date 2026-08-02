f01421f6263bbd55ad16f265cc24e40ee88c4ea4
UpperRacing <zeitlhofer.peter@gmail.com>
2026-08-02 21:21:15 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - Service Worker für GitHub Pages & Offline-Caching
-const CACHE_NAME = 'upperracing-v7.9';
+const CACHE_NAME = 'upperracing-v8.0';
 const assetsToCache = [
   './',
   './index.html',
